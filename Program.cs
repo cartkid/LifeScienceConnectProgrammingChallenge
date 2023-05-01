7b4f02fcb5a045e038ecc47c9b536fe77220dfe2
cartkid <ericlevicky@gmail.com>
2023-05-01 13:19:57 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http.Headers;
+
+using HttpClient client = new HttpClient();
+client.DefaultRequestHeaders.Accept.Clear();
+
+await DoWorkAsync(client);
+
+static async Task DoWorkAsync(HttpClient client)
+{
+
+    var result = await client.GetStreamAsync()
+}
+
+Console.WriteLine("Hello, World!");
